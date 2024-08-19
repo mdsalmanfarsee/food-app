821dec20ebeff90234fd2ba626a8401d38294420
mdsalmanfarsee <far90986@gmail.com>
2024-08-19 17:59:33 +0530

    first-commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import 'dotenv/config'
+
+//add config
+const app = express();
+
+const PORT = process.env.PORT || 4000
+//add middlewire
+app.use(express.json())
+app.use(cors())
+
+//connect database
+connectDB();
+
+//api endpoint
+app.use('/api/food', foodRouter)
+app.use('/images', express.static('uploads'))
+app.use('/api/user', userRouter)
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter)
+
+app.get('/', (req, res) => {
+    res.send("API working")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port http://localhost:${PORT}`);
+})
