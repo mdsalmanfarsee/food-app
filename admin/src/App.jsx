6b4bbf671ccbdd958415b327109eae12d1b166de
mdsalmanfarsee <far90986@gmail.com>
2024-08-19 21:49:25 +0530

    fix admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  const url = 'http://localhost:4000'
+  const url = 'https://foodhut-backend-rbo7.onrender.com'
   return (
     <div>
       <ToastContainer />
