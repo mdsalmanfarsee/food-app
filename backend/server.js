28c41ed22e0294f9ae6eba056a6a3873b633fd45
MD SALMAN FARSEE <far90986@gmail.com>
2024-09-02 11:00:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 app.use(cors(
     {
-    origin:["https://food-app-frontend-tau.vercel.app"],
+    origin:[""],
     methods:["POST","GET"],
     credentials:true
 }
